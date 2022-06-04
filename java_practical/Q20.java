200317c0743cc65fcc8df034a086203c712858f5
Dhruv Soni <81878722+quickcodes@users.noreply.github.com>
2022-06-04 08:42:32 +0530

    Add files via upload

---
+++ java_practical/Q20.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.Scanner;
+public class Q20{
+    public static void main(String[] args){
+        try{
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter your name: ");
+            String name = sc.nextLine();
+
+            BufferedReader br = new BufferedReader(new FileReader("Q19.txt"));
+            String line = br.readLine();
+            while(line != null){
+                if(line.contains("Name")){
+                    System.out.println("{\n");
+                    System.out.println(line);
+                    line = br.readLine();
+                    System.out.println(line);
+                    line = br.readLine();
+                    System.out.println(line);
+                    line = br.readLine();
+                    System.out.println(line);
+                    System.out.println("}\n");
+                }
+                line = br.readLine();
+            }
+            br.close();
+        }catch(IOException e){
+            System.out.println("Error2: " + e);
+        }
+    }
+}
+
+// Output ->
+// Enter your name: Bill
+// {
+
+//         Name: Bill Gates,
+//         Address: America,
+//         Number: 123456789,
+//         ID: 21,
+// }
